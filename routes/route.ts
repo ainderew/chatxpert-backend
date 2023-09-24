a070b855092209dd118d835f2394237e3b1710c6
suniason <galvezbrett17@gmail.com>
2023-09-24 20:02:18 +0800

    changed some for the data management section for datafile

--- routes/route.ts
+++ routes/route.ts
@@ -1,11 +1,8 @@
 import express from 'express'
-import multer from 'multer'
 import CustomerController from '../Controllers/Customer.controller'
 import BusinessController from '../Controllers/Business.controller'
 import DatafileController from '../Controllers/Datafile.controller'
 
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage })
 const route = express()
 const customer = new CustomerController()
 const business = new BusinessController()
@@ -13,7 +10,7 @@ const datafile = new DatafileController()
 
 route.post('/api/register/customer', customer.registerCustomer)
 route.post('/api/register/business', business.registerBusiness)
-route.post('/api/file/upload', upload.single('file'), datafile.fileUpload)
+route.post('/api/file/fileupload', datafile.saveUploadData)
 route.get('/api/file/getallfiles/:businessId', datafile.getAllFiles)
 route.get('/api/file/download/:datafileId', datafile.fileDownload)
 route.post('/api/file/trigger/:datafileId', datafile.setActive)
