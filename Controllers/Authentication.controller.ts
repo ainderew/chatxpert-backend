933b7ab4aba62755a2faa92c78041aac4c8dcbf0
Andrew Pinon <andrewpinon@192.168.254.107>
2023-10-07 05:14:27 +0800

    fix authentication

--- Controllers/Authentication.controller.ts
+++ Controllers/Authentication.controller.ts
@@ -1,56 +1,71 @@
 import { generateAuthToken } from "../Middleware/authentication";
 import Channel from "../Models/Channel.model";
 import { MongoDBUser } from "../Models/User.model";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
-class AuthenticationController{
+class AuthenticationController {
   private model: any;
 
-  constructor(model: any){
-    this.model = model
+  constructor(model: any) {
+    this.model = model;
   }
 
+  public loginUser = async (req: any, res: any): Promise<void> => {
+    const { email, password } = req.body;
 
-  public loginUser = async(req: any,res:any):Promise<void> => {
-    const { email, password} = req.body;
+    const result = await MongoDBUser.findOne({ email: email });
+    if (!result) return res.send({ error: "Invalid Credentials" });
 
-    const result = await MongoDBUser.findOne({ email: email })
-    if (!result) return res.send({error:"Invalid Credentials"})
+    const { _id: userid, password: userpassword } = result;
 
-    const {_id: userid, password:userpassword} = result
+    const checkPassword = await bcrypt.compare(password, userpassword);
+    if (!checkPassword) return res.send({ error: "Invalid Credentials" });
 
-    const checkPassword = await bcrypt.compare(password, userpassword)
-    if (!checkPassword) return res.send({error:"Invalid Credentials"})
+    const token = generateAuthToken(userid.toString());
 
-    const token = generateAuthToken(userid.toString())
-
-    if(result){
-      return res.send({profile: result, token: token})
-    }else{
-      return res.send({profile: 'no user found'})
+    if (result) {
+      return res.send({ profile: result, authToken: token });
+    } else {
+      return res.send({ profile: "no user found" });
     }
+  };
 
-  }
-
-  public getRegisterData = async(req: any,res:any):Promise<void> => {
+  public getRegisterData = async (req: any, res: any): Promise<void> => {
     //Get email and password from view
-    const { email, password} = req.body;
+    const { email, password } = req.body;
 
-    this.model.setEmail(email)
-    this.model.setPassword(password)
+    this.model.setEmail(email);
+    this.model.setPassword(password);
     await this.model.createCustomer();
 
     const channelModel = new Channel();
 
-    channelModel.setAccountId(this.model.getCustomerId())
+    channelModel.setAccountId(this.model.getCustomerId());
     await channelModel.createChannel();
 
-    res.send({response: {
-      userId: this.model.getCustomerId(),
-      channelId: channelModel.getChannelId()
-    }})
-  }
+    res.send({
+      response: {
+        userId: this.model.getCustomerId(),
+        channelId: channelModel.getChannelId(),
+      },
+    });
+  };
 
+  public getProfile = async (req: any, res: any): Promise<void> => {
+    const { userId } = req.body;
+    const account = await MongoDBUser.findOne(
+      { _id: userId },
+      { email: 1, _id: 1, type: 1 }
+    ).lean();
+
+    if (!account) {
+      res.status(404).json("User not found");
+    }
+  
+    res.send({
+      profile: account ,
+    });
+  };
 }
 
-export default AuthenticationController
+export default AuthenticationController;
--- routes/route.ts
+++ routes/route.ts
@@ -2,11 +2,17 @@ import express from 'express'
 import CustomerController from '../Controllers/Customer.controller'
 import BusinessController from '../Controllers/Business.controller'
 import DatafileController from '../Controllers/Datafile.controller'
+import AuthenticationController from '../Controllers/Authentication.controller'
+import { authenticate } from '../Middleware/authentication'
+import User from '../Models/User.model'
 
 const route = express()
+const mUser = new User()
+
 const customer = new CustomerController()
 const business = new BusinessController()
 const datafile = new DatafileController()
+const auth = new AuthenticationController(mUser)
 
 route.post('/api/register/customer', customer.registerCustomer)
 route.post('/api/register/business', business.registerBusiness)
@@ -14,5 +20,6 @@ route.post('/api/file/fileupload', datafile.saveUploadData)
 route.get('/api/file/getallfiles/:businessId', datafile.getAllFiles)
 route.get('/api/file/download/:datafileId', datafile.fileDownload)
 route.post('/api/file/trigger/:datafileId', datafile.setActive)
+route.get('/getProfile',authenticate, auth.getProfile)
 
 export default route
