8da8efdce2827a3c91c1bb4c0e77f76797058086
market0v0 <limanafive@gmail.com>
2023-10-10 19:41:18 +0800

    clicks and age/dob

--- CronJobs/initializeCronJobs.ts
+++ CronJobs/initializeCronJobs.ts
@@ -3,7 +3,6 @@ import { jobType } from '../utils/types'
 import { scheduledJobsLibrary } from './ScheduledJobsLibrary'
 
 export default function initializeScheduledJobs() {
-  console.log('initialize')
   scheduledJobsLibrary.forEach((job: jobType) => {
     cron.schedule(job.cronPattern, job.function)
   })
